dd8e238683f2f050313c3af19d7dd783b8ef5d81
wallisonstorck <storck779@gmail.com>
2021-09-26 18:54:26 -0400

    Adicionando o produto no carrinho de compras

--- script.js
+++ script.js
@@ -27,13 +27,14 @@ function createProductItemElement({ sku, name, image }) {
   
   return section;
 }
-
+// --------------------------------------------------------------------------
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  console.log(event);
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -44,15 +45,33 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
+async function addProductToCart(event) {
+  // console.log(event);
+  // console.log(event.path); // PROVISÓRIO P/ PEGAR O ELEMENTO, DESFAZER ESSA GAMBIARRA
+  const item = event.path[1]; // LEMBRAR DE MUDAR ISSO
+  const skuItem = getSkuFromProductItem(item); // Busca pelo "id" ou "sku" do item.
+  const itemData = await fetch(`https://api.mercadolibre.com/items/${skuItem}`); // Faz requisição a API com identificação do item
+  const itemDataInJson = await itemData.json(); // Transforma os dados da requisição em JSON
+  const productItem = { // Separa os dados necessários do item
+    sku: itemDataInJson.id,
+    name: itemDataInJson.title,
+    salePrice: itemDataInJson.price,
+  };
+  const ol = document.querySelector('.cart__items'); // Mapeia a ol que vai abrigar os itens do carrinho
+  ol.appendChild(createCartItemElement(productItem)); // Anexa o item criado na ol
+}
+
 function productListing(items) { // Mapeia a section e anexa os produtos
   const sectionItems = document.querySelector('.items'); // Aponta para a section
   items.forEach((item) => { // Percorre todos os itens adicionando-os a section
     const element = createProductItemElement(item);
-    sectionItems.appendChild(element);
+    element.querySelector('button').addEventListener('click', addProductToCart); // Adiciona um evento de click já que o botão não tem "onclick"
+    sectionItems.appendChild(element); // Anexa o elemento criado a section
   });
+  // console.log(sectionItems);
 }
 
-async function getProductAPI() { // Carrega os dados do endpoint e guarda na variavel global
+async function getProductAPI() { // Carrega os dados do endpoint.
   try {
     const dataLoad = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador'); // Dados "brutos"
     const dataInJson = await dataLoad.json(); // Dados em formato json
